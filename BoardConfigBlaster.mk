47ed560624e4b0a5d037ace605f980625da812ad
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-05 05:07:40 +0530

    Add device manifest for radio & keymint
    Change-Id: Ib6c4b28d33fb64cb05c715d86a96a829a498beac

--- BoardConfigBlaster.mk
+++ BoardConfigBlaster.mk
@@ -41,3 +41,8 @@ BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
+
+# Manifests
+DEVICE_MANIFEST_FILE += \
+    device/google/gs201/android.hardware.security.rkp-service.citadel.xml \
+    device/google/gs201/manifest_radio_ds.xml
